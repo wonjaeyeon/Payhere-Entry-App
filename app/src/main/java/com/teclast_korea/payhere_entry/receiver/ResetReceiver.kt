34ff01ea8e27c3889d87391d90e71d273bc0dcb8
wonjaeyeon <woncow977@gmail.com>
2025-03-10 14:40:09 +0900

    25.03.10 : release_v1.1.0_250310 to payhere : feature_v1.1.0_custom_start_screen added

--- app/src/main/java/com/teclast_korea/payhere_entry/receiver/ResetReceiver.kt
+++ app/src/main/java/com/teclast_korea/payhere_entry/receiver/ResetReceiver.kt
@@ -12,49 +12,6 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
-//@AndroidEntryPoint
-//class ResetReceiver : BroadcastReceiver() {
-//
-//    @Inject
-//    lateinit var repository: SelectedAppRepository
-//
-//    override fun onReceive(context: Context, intent: Intent) {
-//        when (intent.action) {
-//            ACTION_RESET -> {
-//                // Clear the DB entry on an IO thread
-//                CoroutineScope(Dispatchers.IO).launch {
-//                    repository.clearSelectedApp()
-//                    Log.i("ResetReceiver", "Cleared selected app")
-//                }
-//            }
-//
-//            ACTION_SET_HOME_APP -> {
-//                // We'll read the package name from an extra. Example: "packageName"
-//                val packageName = intent.getStringExtra(EXTRA_PACKAGE_NAME)
-//                if (!packageName.isNullOrEmpty()) {
-//                    CoroutineScope(Dispatchers.IO).launch {
-//                        repository.setSelectedApp(packageName)
-//                        Log.i("ResetReceiver", "Set selected app to $packageName")
-//                    }
-//                } else {
-//                    Log.w("ResetReceiver", "No packageName extra found in ACTION_SET_HOME_APP broadcast.")
-//                }
-//            }
-//        }
-//    }
-//
-//    companion object {
-//        // Unique actions
-//        const val ACTION_RESET = "ACTION_RESET"
-//        const val ACTION_SET_HOME_APP = "ACTION_SET_HOME_APP"
-//
-//        // Key for the package name extra
-//        const val EXTRA_PACKAGE_NAME = "packageName"
-//    }
-//}
-//
-
-
 @AndroidEntryPoint
 class ResetReceiver : BroadcastReceiver() {
 
